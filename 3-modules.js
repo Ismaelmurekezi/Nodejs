52ea49c3636c14d9f2d938e17ba758fe38268533
ismaelmurekezi <ismaelmurekezi1@gmail.com>
2024-02-14 23:27:56 +0200

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+//Modules
+const name=require('./4-names')
+const sayHi=require('./5-utils')
+const data=require('./6-alternatives')
+require('./7-mind-grenade');
+
+
+sayHi('Susan');
+sayHi(name.john);
+sayHi(name.peter);
